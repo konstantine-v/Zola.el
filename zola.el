81594bff7ebe8c9ae607e283c144ee8b3f8ae18c
MaterialFuture <konstantine@andrasmedia.org>
2020-08-19 23:09:53 -0500

    init

---
+++ zola.el
@@ -0,0 +1,23 @@
+;;; zola.el --- description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 MaterialFuture
+;;
+;; Author: MaterialFuture <http://github/MaterialFuture>
+;; Maintainer: MaterialFuture <http://github/MaterialFuture>
+;; Created: August 18, 2020
+;; Modified: August 18, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/MaterialFuture/zola
+;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  description
+;;
+;;; Code:
+
+(provide 'zola)
+;;; zola.el ends here
