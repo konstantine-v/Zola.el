fb4b94ac435dc45a205b5704367f58772cd4e003
MaterialFuture <konstantine@andrasmedia.org>
2020-08-22 23:58:42 -0500

    working startup and teardown of local server

--- zola.el
+++ zola.el
@@ -19,7 +19,6 @@
 ;;
 ;;; Code:
 
-;;;###autoload
 (defgroup zola nil
   "Support for Zola static site generator."
   :tag "zola"
@@ -31,22 +30,52 @@
   :tag "Zola Executable"
   :type 'string)
 
+(defcustom zola-default-server-url "127.0.0.1"
+  "Local URL of Zola HTTP server (zola serve)."
+  :group 'zola
+  :tag "Default Zola Web Server URL"
+  :type 'string)
+
+(defcustom zola-default-server-port 1111
+  "Port number of Zola HTTP server (zola serve)."
+  :group 'zola
+  :tag "Default Zola Web Server Port"
+  :type 'integer)
+
+(defvar buff-command "zola serve")
+(defvar buff-name nil)
+
 ;;;###autoload
-(defun zola-run-zola-serve (hostname port &optional project-dir) ;TODO: Rename function to something better
+(defun zola-start-server (hostname port)
   "Run Zola with zola serve in minibuffer.
 `HOSTNAME': Hostname or IP address you want the web server to be.
 `PORT': Port to be used to zola serve.
-`PROJECT-DIR': Path to Project root directory.
 
 Project root directory is found via Magit when none is specified."
   (interactive
    (list
-    (read-string "Hostname: " "127.0.0.1")
-    (read-number "Port: " 1111)))
-  (shell-command-to-string (format "zola serve -u %s -p %d" hostname port)))
-;; NOTE: Zola server starts but hangs after it successfully starts the server.
-;; TODO: Add confirmation (message) to the buffer to let user know what is going on
-;; TODO: Add command to shutdown server like (defun zola-stop-server)
+    (read-string "Hostname: " zola-default-server-url)
+    (read-number "Port: " zola-default-server-port)))
+  (setq buff-command (format "zola serve -u %s -p %s" hostname port))
+  (setq buff-name (format "Zola Server on %s:%s" hostname port))
+  (start-process-shell-command
+   "Start server"
+   buff-name
+   buff-command))
+;; TODO: Refactor the (start-process-shell-command)
+
+;;;###autoload
+(defun zola-stop-server (&optional process)
+  "Kill the Zola PROCESS that you started with (zola-start-server) or otherwise."
+  (interactive
+   (list
+    (read-string "Process: " "zola")))
+  (setq buff-command (format "pkill %s" process))
+  (setq buff-name "Killed Process")
+  (start-process-shell-command
+   "Stop Process"
+   buff-name
+   buff-command))
 
 (provide 'zola)
 ;;; zola.el ends here
